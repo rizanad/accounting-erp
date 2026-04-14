d1248b56322891d0db60aa6fa71ca217d9a3a038
Rizan Adhikari <adhikaririzan9@gmail.com>
2026-04-15 00:20:30 +0545

    bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 1. Import BrowserRouter
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
